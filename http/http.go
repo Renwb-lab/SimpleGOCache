d25f3652ee3d2fdc50321ca7cf91719c827a1c91
Renwb-lab <1534609090@qq.com>
2023-10-23 23:07:21 +0800

    [day5] 分布式节点

--- http/http.go
+++ http/http.go
@@ -2,19 +2,27 @@ package http
 
 import (
 	"SimpleGoCache/cache"
+	"SimpleGoCache/consistenthash"
 	"fmt"
 	"log"
 	"net/http"
 	"strings"
+	"sync"
 )
 
-const defaultBasePath = "/_geecache/"
+const (
+	defaultBasePath = "/_geecache/"
+	defaultReplicas = 50
+)
 
 // HTTPPool implements PeerPicker for a pool of HTTP peers.
 type HTTPPool struct {
 	// this peer's base URL, e.g. "https://example.net:8000"
-	self     string
-	basePath string
+	self        string
+	basePath    string
+	mu          sync.Mutex // guards peers and httpGetters
+	peers       *consistenthash.Map
+	httpGetters map[string]cache.PeerGetter // keyed by e.g. "http://10.0.0.2:8008"
 }
 
 // NewHTTPPool initializes an HTTP pool of peers.
@@ -61,3 +69,28 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/octet-stream")
 	w.Write(view.ByteSlice())
 }
+
+// Set updates the pool's list of peers.
+func (p *HTTPPool) Set(peers ...string) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.peers = consistenthash.New(defaultReplicas, nil)
+	p.peers.Add(peers...)
+	p.httpGetters = make(map[string]cache.PeerGetter, len(peers))
+	for _, peer := range peers {
+		p.httpGetters[peer] = cache.NewHttpGetter(peer + p.basePath)
+	}
+}
+
+// PickPeer picks a peer according to key
+func (p *HTTPPool) PickPeer(key string) (cache.PeerGetter, bool) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	if peer := p.peers.Get(key); peer != "" && peer != p.self {
+		p.Log("Pick peer %s", peer)
+		return p.httpGetters[peer], true
+	}
+	return nil, false
+}
+
+var _ cache.PeerPicker = (*HTTPPool)(nil)
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"SimpleGoCache/cache"
 	http2 "SimpleGoCache/http"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -14,8 +15,8 @@ var db = map[string]string{
 	"Sam":  "567",
 }
 
-func main() {
-	cache.NewGroup("scores", 2<<10, cache.GetterFunc(
+func createGroup() *cache.Group {
+	return cache.NewGroup("scores", 2<<10, cache.GetterFunc(
 		func(key string) ([]byte, error) {
 			log.Println("[SlowDB] search key", key)
 			if v, ok := db[key]; ok {
@@ -23,9 +24,60 @@ func main() {
 			}
 			return nil, fmt.Errorf("%s not exist", key)
 		}))
+}
 
-	addr := "localhost:9999"
+func startCacheServer(addr string, addrs []string, gee *cache.Group) {
 	peers := http2.NewHTTPPool(addr)
+	peers.Set(addrs...)
+	gee.RegisterPeers(peers)
 	log.Println("geecache is running at", addr)
-	log.Fatal(http.ListenAndServe(addr, peers))
+	log.Fatal(http.ListenAndServe(addr[7:], peers))
+}
+
+func startAPIServer(apiAddr string, gee *cache.Group) {
+	http.Handle("/api", http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			key := r.URL.Query().Get("key")
+			view, err := gee.Get(key)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/octet-stream")
+			w.Write(view.ByteSlice())
+
+		}))
+	log.Println("fontend server is running at", apiAddr)
+	log.Fatal(http.ListenAndServe(apiAddr[7:], nil))
+}
+
+func main() {
+	var port int
+	var api bool
+	flag.IntVar(&port, "port", 8001, "Geecache server port")
+	flag.BoolVar(&api, "api", false, "Start a api server?")
+	flag.Parse()
+
+	apiAddr := "http://localhost:9999"
+	addrMap := map[int]string{
+		8001: "http://localhost:8001",
+		8002: "http://localhost:8002",
+		8003: "http://localhost:8003",
+	}
+
+	var addrs []string
+	for _, v := range addrMap {
+		addrs = append(addrs, v)
+	}
+
+	gee := createGroup()
+	if api {
+		// 用来启动一个 API 服务（端口 9999），与用户进行交互
+		go startAPIServer(apiAddr, gee)
+	}
+	// 用来启动缓存服务器，多次启动
+	// ./server -port=8001
+	// ./server -port=8002
+	// ./server -port=8003
+	startCacheServer(addrMap[port], addrs, gee)
 }
